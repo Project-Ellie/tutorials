0f4171c834b995006abb48758c6b4e0103c51e9b
Wolfgang Giersche <wgiersche@gmail.com>
2018-11-21 12:50:22 +0000

    cosmetic improvements

--- kovalevskyi/run_task.sh
+++ kovalevskyi/run_task.sh
@@ -15,7 +15,7 @@ python -m train.task \
   --hypothesis="linear"  \
   --model_dir="gs://going-tfx/samples/model"  \
   --train_batch_size="256"  \
-  --max_train_steps="2000"  \
+  --max_train_steps="1000"  \
   --metadata_dir="gs://going-tfx/samples/metadata"  \
   --train_data_pattern="gs://going-tfx/samples/train_data/atl_june_tfr*"  \
   --save_checkpoints_steps="2000"  \
