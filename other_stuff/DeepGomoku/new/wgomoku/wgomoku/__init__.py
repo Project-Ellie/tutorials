635c3d7e692fdc9d755b07ec5ae7ffe4d5c600f8
Wolfgang Giersche <wgiersche@gmail.com>
2019-06-09 19:40:11 +0000

    Most notebooks now on wgomoku

--- other_stuff/DeepGomoku/new/wgomoku/wgomoku/__init__.py
+++ other_stuff/DeepGomoku/new/wgomoku/wgomoku/__init__.py
@@ -1,6 +1,12 @@
 from GomokuBoard import GomokuBoard
-from HeuristicPolicy import HeuristicGomokuPolicy
+from HeuristicPolicy import (
+    HeuristicGomokuPolicy, ThreatSearch, Move, StochasticMaxSampler)
 from GomokuTools import GomokuTools
 from Heuristics import Heuristics
-from GomokuData import record_game
-from UCT_Search import UCT_Node, PolicyAdapter, GomokuEnvironment, GomokuState
+from UCT_Search import UCT_Node, PolicyAdapter, GomokuEnvironment, GomokuState, uct_search
+from NH9x9 import NH9x9
+from GomokuData import (
+    roll_out, variants_for, transform, create_sample,
+    wrap_sample, create_samples_and_qvalues, data_from_game,
+    to_matrix12, to_matrix_xo)
+from QFunction import heuristic_QF
