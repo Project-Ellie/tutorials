f038ad771994d9a03986947571586669a5c2cd05
Wolfgang Giersche <wgiersche@gmail.com>
2019-06-09 19:43:10 +0200

    wgomoku to be finalized on GCP

---
+++ other_stuff/DeepGomoku/new/wgomoku/wgomoku/__init__.py
@@ -0,0 +1,6 @@
+from GomokuBoard import GomokuBoard
+from HeuristicPolicy import HeuristicGomokuPolicy
+from GomokuTools import GomokuTools
+from Heuristics import Heuristics
+from GomokuData import record_game
+from UCT_Search import UCT_Node, PolicyAdapter, GomokuEnvironment, GomokuState
